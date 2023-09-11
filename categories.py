e8bcd53c1922a8370b0db15bdec58da68f4fa1d2
Hananeh Montazer <78915560+Hananehmo@users.noreply.github.com>
2023-09-11 12:16:44 +0330

    find subcategories added

--- categories.py
+++ categories.py
@@ -26,3 +26,22 @@ def find_category():
         new_array = [element]
         new_arrays.append(new_array)
     return new_arrays
+
+
+def find_subcategory(new_arrays):
+    def check_and_write(text: str, arr):
+        if text.startswith("/subcategory"):
+            arr.append("https://basalam.com" + text)
+
+    for j in range(len(new_arrays)):
+        url = new_arrays[j][0]
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        links: List[Tag] = soup.find_all('a')
+        specific_links = []
+        for link in links:
+            if link['href'] in specific_links:
+                continue
+            specific_links.append(link['href'])
+            check_and_write(link['href'], new_arrays[j])
