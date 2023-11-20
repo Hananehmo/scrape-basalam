4eb32f50d9628aac4832a4978b1b75f1943a0200
Hananehmo <hnnemntr200@gmail.com>
2023-11-20 14:37:00 +0330

    import os

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import categories
-
+import os
 new_arrays = categories.find_category()
 res = categories.find_subcategory(new_arrays)
 res = categories.find_under_subcategory(res)
 categories.find_products_and_images(res)
+
